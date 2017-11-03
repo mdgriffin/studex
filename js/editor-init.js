2dbccbfe7303588a2a8404e03216fac792c03cd3
Michael Griffin <mdgriffin064@gmail.com>
2017-11-03 18:03:57 +0000

    Added logging to check if script is loaded

--- js/editor-init.js
+++ js/editor-init.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("Initializing editor");
   var simplemde = new SimpleMDE({ element: document.getElementById("markdownEditor") });
 });
